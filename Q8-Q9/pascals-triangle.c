5b79386ac25a06ea8177b5b043b7e121165a9f9b
Rohit <tprohit9@gmail.com>
2021-07-26 14:31:52 +0530

    updated algoirthm

--- Q8-Q9/pascals-triangle.c
+++ Q8-Q9/pascals-triangle.c
@@ -1,13 +1,5 @@
 #include <stdio.h>
 
-int factorial(int n, int factorials[])
-{
-    if(!factorials[n])
-        factorials[n] = factorial(n-1, factorials) * n;   
-
-    return factorials[n];
-}
-
 int main()
 {
    int rows;
@@ -18,26 +10,19 @@ int main()
    if(rows < 1)
     return printf("No rows to print\n"), 1;
 
-   int factorials[rows+3];
-   factorials[0] = 1;
-   factorials[1] = 1;
+    char tabs[rows];
 
-   char tabs[rows + 1];
-
-    for(int i=0; i< rows+1; i++) 
-    {
-        factorials[i+2] = 0;
+    for(int i=0; i< rows; i++) 
         tabs[i] = ' ';
+
+    for (int line = 1; line <= rows; line++)
+    {
+        printf("%s",tabs);
+        tabs[rows - line] = '\0';
+
+        for (int i = 1,C = 1; i <= line; C = (C * (line - i)) / i++)
+            printf("%d ", C);
+
+        printf("\n");
     }
-
-   for(int i=0; i< rows; i++)
-   {
-       printf("%s",tabs);
-       tabs[rows - i] = '\0';
-
-        for(int j=0; j<=i; j++)
-            printf("%d ",factorial(i,factorials)/factorial(i-j,factorials)/factorial(j,factorials));
-
-        printf("\n");    
-   }
 }
