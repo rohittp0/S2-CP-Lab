b0d4806e674dfedc9ef161c6a686a886fdbc1980
Rohit <tprohit9@gmail.com>
2021-07-26 15:22:31 +0530

    bugfix

--- Q8-Q9/pascals-triangle.c
+++ Q8-Q9/pascals-triangle.c
@@ -11,18 +11,18 @@ int main()
     return printf("No rows to print\n"), 1;
 
     char tabs[rows];
-
-    for(int i=0; i< rows; i++) 
-        tabs[i] = ' ';
+    for(int i=0; i< rows; tabs[i++] = ' ');
 
     for (int line = 1; line <= rows; line++)
     {
         printf("%s",tabs);
         tabs[rows - line] = '\0';
 
-        for (int i = 1,C = 1; i <= line; C = (C * (line - i)) / i++)
-            printf("%d ", C);
+        for (int i = 1, n= 1; i <= line; n = (n * (line - i)) / i++)
+            printf("%d ", n);
 
         printf("\n");
     }
+
+    return 0;
 }
